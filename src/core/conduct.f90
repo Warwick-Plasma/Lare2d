daf7e0763a414dd754ce965ce830507d74fe7000
Tony Arber <Tony@ac-87-a3-0c-0d-7b.warwick.ac.uk>
2017-05-24 11:50:33 +0100

    Updated conduction routines and note. Started on the radiation and heating terms.

--- src/core/conduct.f90
+++ src/core/conduct.f90
@@ -287,62 +287,68 @@ CONTAINS
 
 
 
-!   SUBROUTINE rad_losses(density, temperature, xi, height, rad)
+  SUBROUTINE rad_losses(density, temperature, xi, height, rad)
 
-!     ! Returns the normalised RTV losses
+    ! Returns the normalised RTV losses
+    ! Input density is the mass density 
+    ! Both density and temperature are in Lare2d normalised units
+    ! To replace with a different loss function note that this
+    ! routine first calculates the S.I. loss rate as defined in the manual.
+    ! This is then converted to LareXd units by multiplying by h_star
 
-!     REAL(num), INTENT(IN) :: density, temperature, xi, height
-!     REAL(num), INTENT(OUT) :: rad
-!     REAL(num), DIMENSION(7) :: trange = (/0.02_num, 0.0398_num, 0.0794_num, &
-!         0.251_num, 0.562_num, 1.995_num, 10.0_num/)
-!     REAL(num), DIMENSION(6) :: psi = (/1.2303_num, 870.96_num, 5.496_num, &
-!         0.3467_num, 1.0_num, 1.6218_num/)
-!     REAL(num), DIMENSION(6) :: alpha = (/0.0_num, 2.0_num, 0.0_num, &
-!         -2.0_num, 0.0_num, -2.0_num / 3.0_num/)
-!     REAL(num) :: tmk, factor
-!     INTEGER :: i
+    REAL(num), INTENT(IN) :: density, temperature, xi, height
+    REAL(num), INTENT(OUT) :: rad
+    REAL(num), DIMENSION(7) :: trange = (/0.02_num, 0.0398_num, 0.0794_num, &
+        0.251_num, 0.562_num, 1.995_num, 10.0_num/)
+    REAL(num), DIMENSION(6) :: psi = (/1.2303_num, 870.96_num, 5.496_num, &
+        0.3467_num, 1.0_num, 1.6218_num/)
+    REAL(num), DIMENSION(6) :: alpha = (/0.0_num, 2.0_num, 0.0_num, &
+        -2.0_num, 0.0_num, -2.0_num / 3.0_num/)
+    REAL(num) :: tmk, factor
+    INTEGER :: i
 
-!     rad = 0.0_num
+    rad = 0.0_num
 
-!     IF (.NOT. radiation) RETURN
-!     IF (height < 1.0_num) RETURN
+    IF (.NOT. radiation) RETURN
+    IF (height < 1.0_num) RETURN
 
-!     tmk = temperature * t2tmk
-!     IF (tmk < trange(1) .OR. tmk > trange(7)) RETURN
+    tmk = temperature * t2tmk
+    IF (tmk < trange(1) .OR. tmk > trange(7)) RETURN
 
-!     DO i = 1, 6
-!       IF (tmk >= trange(i) .AND. tmk <= trange(i+1)) EXIT
-!     END DO
+    DO i = 1, 6
+      IF (tmk >= trange(i) .AND. tmk <= trange(i+1)) EXIT
+    END DO
 
-!     ! Account for reduced electron number density due to neutrals
-!     factor = (1.0_num - xi)**2
-!     IF (eos_number == EOS_IDEAL) factor = 1.0_num
+    ! Account for reduced electron number density due to neutrals
+    factor = (1.0_num - xi)**2
+    IF (eos_number == EOS_IDEAL) factor = 1.0_num
 
-!     rad = factor * density**2 * psi(i) * tmk**alpha(i)
-!     rad = rad * h_star * lr_star
+    rad = factor * density**2 * psi(i) * tmk**alpha(i)
+    ! Determine the radiative losses in S.I. units
+    rad = rad * lr_star
 
-!   END SUBROUTINE rad_losses
+    ! Convert S.I. unit radiative losses to LareXd normalised value
+    rad = rad * h_star 
+
+  END SUBROUTINE rad_losses
 
 
 
-  FUNCTION heating(density, t0)
+  FUNCTION heating(density, temperature)
 
     ! For a given density and temperature returns a user specific
-    ! heating function
+    ! heating function.
+    ! Input density is the mass density. 
+    ! Both density and temperature are in Lare2d normalised units
 
-    REAL(num), INTENT(IN) :: density, t0
+    REAL(num), INTENT(IN) :: density, temperature
     REAL(num) :: heating
-    REAL(num) :: tmk
-    REAL(num) :: heat0 = 0.0_num
-    REAL(num) :: rho_coronal = 0.0_num
 
+    ! First specify the heating in S.I. units
     heating = 0.0_num
-    IF (.NOT. coronal_heating) RETURN
 
-    tmk = t0 * t2tmk
-    ! For low density and high temeprature define a heating course term
-    IF (density < rho_coronal .AND. tmk > 0.02_num) &
-        heating = 100.0_num * heat0 * density**2
+    ! Convert to LareXd normalised units
+    heating = heating * h_star
 
   END FUNCTION heating
 
